186a85a87b5403f23bd39f22c0d3d76f4a83774d
SirDragons <165951719+SirDragons@users.noreply.github.com>
2024-11-30 12:27:23 -0500

    Create hangmanstages.py
    Create the stages in the hangman minigame.

---
+++ hangmanstages.py
@@ -0,0 +1,89 @@
+def stage9():
+	print()
+	print()
+	print()
+	print()
+	print()
+	print("|")
+def stage8():
+	print("__")
+	print("|/")
+	print("|")
+	print("|")
+	print("|")
+	print("|")
+def stage7():
+	print("_________")
+	print("|/")
+	print("|")
+	print("|")
+	print("|")
+	print("|")
+def stage6():
+	print("_________")
+	print("|/	 |")
+	print("|")
+	print("|")
+	print("|")
+	print("|")
+def stage5():
+	print("_________")
+	print("|/	 |")
+	print("|	 O")
+	print("|")
+	print("|")
+	print("|")
+def stage4():
+	print("_________")
+	print("|/	 |")
+	print("|	 O")
+	print("|	/|")
+	print("|")
+	print("|")
+def stage3():
+	print("_________")
+	print("|/	 |")
+	print("|	 O")
+	print("|	/|\\")
+	print("|")
+	print("|")
+def stage2():
+	print("_________")
+	print("|/	 |")
+	print("|	 O")
+	print("|	/|\\")
+	print("|	/ ")
+	print("|")
+def stage1():
+	print("_________")
+	print("|/	 |")
+	print("|	 O")
+	print("|	/|\\")
+	print("|   / \\")
+	print("|")
+def stage0():
+	print("_________")
+	print("|/	 |")
+	print("|	 O")
+	print("|	/|\\")
+	print("|   / \\")
+	print("|    *")
+def checkStage(chances):
+	if chances == 9:
+		stage9()
+	if chances == 8:
+		stage8()
+	if chances == 7:
+		stage7()
+	if chances == 6:
+		stage6()
+	if chances == 5:
+		stage5()
+	if chances == 4:
+		stage4()
+	if chances == 3:
+		stage3()
+	if chances == 2:
+		stage2()
+	if chances == 1:
+		stage1()
